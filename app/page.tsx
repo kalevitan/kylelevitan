138d6a549923272abbca8ba68b3a22a171d57997
Kyle Levitan <kylelevitan@gmail.com>
2024-11-07 11:03:02 -0500

    Remove portfolio for build testing

--- app/page.tsx
+++ app/page.tsx
@@ -27,9 +27,9 @@ export default function Home() {
           <Header/>
         </Section>
 
-        <Section title="portfolio">
+        {/* <Section title="portfolio">
           <Portfolio/>
-        </Section>
+        </Section> */}
 
         <Section title="credentials">
           <Credentials/>
