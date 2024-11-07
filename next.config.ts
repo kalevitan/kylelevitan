c1ba314f23772ad8845a6c9acbd943d9047ecd3d
Kyle Levitan <kylelevitan@gmail.com>
2024-11-06 22:17:32 -0500

    Adding a disDir for cloudflare

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Additional config options you may need
+  distDir: 'build',
 };
 
 export default nextConfig;
