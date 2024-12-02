e717c3f699b9e1db0474d62f61956d508118b972
Kyle Levitan <kylelevitan@gmail.com>
2024-12-01 20:46:19 -0500

    Added bikelock finder app, added leaflet presentation

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 const Header: React.FC = () => {
-  return(
+  return (
     <div className="layout">
       <h1>
         <span className="header__name">Kyle Levitan</span><br/>
--- app/components/presentations.tsx
+++ app/components/presentations.tsx
@@ -8,6 +8,7 @@ const Presentations: React.FC = () => {
        <ul className="presentations__links unstyled">
         <li><a href="https://www.youtube.com/watch?v=8JLEYSqWKb0" rel="noreferrer" target="_blank" title="MyGreat Migrate Experience">My-Great Migrate Experience</a>&nbsp;<span>|&nbsp;Drupal Camp Asheville 2018</span></li>
         <li><a href="https://www.meetup.com/asheville-drupal-user-group/events/243651675/" rel="noreferrer" target="_blank" title="Drupal Frontend Development with Pattern Lab">Drupal Frontend Development with Pattern Lab</a>&nbsp;<span>|&nbsp;ADUG Meetup 2017</span></li>
+        <li><a href="/presentations/leveraging-leafletjs-with-drupal8/index.html" rel="noreferrer" target="_blank" title="Leveraging LeafletJS with Drupal 8">Leveraging LeafletJS with Drupal 8</a>&nbsp;<span>|&nbsp;Drupal Camp Asheville 2016</span></li>
       </ul>
     </div>
   )
