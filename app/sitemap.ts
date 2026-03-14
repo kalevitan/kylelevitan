8c5c19e6b3b78049d0a7b737d813405bdf590eb4
Kyle Levitan <kylelevitan@gmail.com>
2026-03-14 12:08:55 -0400

    Make sitemap dynamic

---
+++ app/sitemap.ts
@@ -0,0 +1,35 @@
+import { MetadataRoute } from "next";
+import { caseStudies } from "./components/portfolio/case-studies";
+import { SEO_CONFIG } from "./lib/seo";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const staticRoutes: MetadataRoute.Sitemap = [
+    {
+      url: SEO_CONFIG.siteUrl,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1.0,
+    },
+    {
+      url: `${SEO_CONFIG.siteUrl}/#portfolio`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: `${SEO_CONFIG.siteUrl}/#skills`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+  ];
+
+  const caseStudyRoutes: MetadataRoute.Sitemap = caseStudies.map((cs) => ({
+    url: `${SEO_CONFIG.siteUrl}/projects/${cs.slug}`,
+    lastModified: new Date(),
+    changeFrequency: "monthly",
+    priority: 0.9,
+  }));
+
+  return [...staticRoutes, ...caseStudyRoutes];
+}
